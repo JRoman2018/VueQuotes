cc0d13d5f38442c2266ceb7c684220f69f015d0f
jose roman <el-1ero@outlook.com>
2021-09-05 11:47:41 -0400

    init app

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <quote-grid :quotes="quotes"></quote-grid>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue';
+export default {
+  components: { QuoteGrid },
+  data() {
+    return { quotes: ['Just a Quote to see something.'], maxQuotes: 10 };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <app-quote v-for="quote in quotes" :key="quote">
+      <p>{{ quote }}</p>
+    </app-quote>
+  </div>
+</template>
+
+<script>
+import Quote from './Quote.vue';
+export default {
+  components: { AppQuote: Quote },
+  props: ['quotes'],
+};
+</script>
+
+<style scoped></style>
